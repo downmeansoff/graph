ab568784bbe084ac860735d4aff05e0ef3f71277
downmeansoff <116756888+downmeansoff@users.noreply.github.com>
2024-06-04 16:14:55 +0400

    added delete vertex

--- include/graph.cc
+++ include/graph.cc
@@ -37,4 +37,38 @@ public:
         _edges.insert({v, {}}); // Создаем пустой список ребер для вершины v и вставляем его в контейнер _edges
     }
 
+    std::vector<Edge> get_incoming_edges(const V &vert) const {
+        // Возвращает вектор входящих ребер для заданной вершины vert
+        std::vector<Edge> incoming_vert; // Создаем вектор для хранения входящих ребер
+        for (const auto &v: _vertices) { // Перебираем все вершины в графе
+            for (const auto &edge: _edges.at(v)) { // Перебираем ребра для текущей вершины v из контейнера _edges
+                if (edge.to == vert) // Проверяем, является ли вершина "to" текущего ребра равной заданной вершине vert
+                    incoming_vert.push_back(edge); // Если да, добавляем это ребро в вектор incoming_vert
+            }
+        }
+        return incoming_vert; // Возвращаем вектор входящих ребер для заданной вершины vert
+    }
+
+    bool remove_vertex(const V &vert) {
+        // Метод удаляет вершину vert из графа
+        if (_vertices.erase(vert)) { // Проверяем, удалось ли удалить вершину vert из контейнера _vertices
+            _edges.erase(vert); // Удаляем все ребра, связанные с вершиной vert из контейнера _edges
+            for (auto edge: get_incoming_edges(vert)) { // Перебираем входящие ребра для вершины vert
+                std::erase_if(_edges[edge.from], [vert](const Edge &edge) {
+                    return edge.to == vert;
+                }); // Удаляем ребра, указывающие на вершину vert
+            }
+            return true; // Возвращаем true, если вершина была успешно удалена
+        }
+        return false; // Возвращаем false, если вершина не была найдена и удалена
+    }
+
+    std::vector<V> vertices() const {
+        // Метод возвращает вектор всех вершин в графе
+        std::vector<V> vertices; // Создаем вектор для хранения вершин
+        for (const auto &vert: _vertices)
+            vertices.push_back(vert); // Добавляем каждую вершину из контейнера _vertices в вектор vertices
+        return vertices; // Возвращаем вектор всех вершин
+    }
+
 };
--- tests/graph_tests.cc
+++ tests/graph_tests.cc
@@ -6,3 +6,11 @@ TEST(graph_test,add_vertex){
     graph.add_vertex(3);
     ASSERT_TRUE(graph.has_vertex(3));
 }
+
+TEST(graph_test,delete_vertex){
+    Graph<int,double> graph;
+    graph.add_vertex(3);
+    ASSERT_TRUE(graph.has_vertex(3));
+    graph.remove_vertex(3);
+    ASSERT_FALSE(graph.has_vertex(3));
+}
